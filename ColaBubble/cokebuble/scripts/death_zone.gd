875401c141b05aff909eae6790833f51e9de7092
nortHades <128273140+nortHades@users.noreply.github.com>
2025-04-12 11:10:55 -0700

    modifications

--- ColaBubble/cokebuble/scripts/death_zone.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node):
-	if body.name == "player":  # 检测是否是角色
-		restart_game()
-
-func restart_game():
-	get_tree().change_scene_to_file("res://scene/game_over.tscn")
